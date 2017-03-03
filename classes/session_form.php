beae3d5a9f49e1adf191d4da6e1d8edaaf78425f
dusan <dusan.sparavalo@lambdasolutions.net>
2017-03-03 16:52:30 +0100

    LCD-933 fixed issue for Moodle 31

--- classes/session_form.php
+++ classes/session_form.php
@@ -232,8 +232,8 @@ class mod_facetoface_session_form extends moodleform {
         $dateids = $data['sessiondateid'];
         $dates = count($dateids);
         for ($i = 0; $i < $dates; $i++) {
-            $starttime = $data["timestart[$i]"];
-            $endtime = $data["timefinish[$i]"];
+            $starttime = $data["timestart"][$i];
+            $endtime = $data["timefinish"][$i];
             $removecheckbox = empty($data["datedelete"]) ? array() : $data["datedelete"];
             if ($starttime > $endtime && !isset($removecheckbox[$i])) {
                 $errstr = get_string('error:sessionstartafterend', 'facetoface');
